e75fa7a1592ddc9985d5cbe0c25494a9cd6b257d
Aryan Malpotra <aryan.malpotra@originscale.io>
2024-07-30 18:18:26 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function CustomNavbar() {
     <Navbar className="bg-gradient-custom py-4">
       <Container>
         <Navbar.Brand href="#">
-        <Image src='/logo.svg' width={150} height={150}/>
+        <Image src='/logo.svg' width={150} height={150} alt='image'/>
         </Navbar.Brand>
         <Navbar.Text className="ml-auto text-3xl text-white font-semibold">
           SME HealthCheck - Get Started
